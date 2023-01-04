44fc57fcd3a4b0d77469a3b44ec8f76408478275
andriy-shymkiv <winamondezik@gmail.com>
2023-01-04 17:59:23 +0200

    init

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import serverless from 'serverless-http';
+
+const router = express.Router();
+
+const app = express();
+
+app.use(cors());
+
+router.get('/', (req, res) => {
+    res.json({
+        'hello': '123',
+    })
+})
+
+app.use('/.netlify/functions/server', router);
+
+export const handler = serverless(app);

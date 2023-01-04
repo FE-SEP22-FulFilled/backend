358d3987eff61820fcac19cf9ee5bc2b8dbfaee6
andriy-shymkiv <winamondezik@gmail.com>
2023-01-04 20:42:47 +0200

    husky test 1

--- src/server.ts
+++ src/server.ts
@@ -9,10 +9,10 @@ const app = express();
 app.use(cors());
 
 router.get('/', (req, res) => {
-    res.json({
-        'hello': '123',
-    })
-})
+  res.json({
+    hello: '12345',
+  });
+});
 
 app.use('/.netlify/functions/server', router);
 

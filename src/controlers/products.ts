10aa657dfe167c82cf562bce480059f1c992b85a
andriy-shymkiv <winamondezik@gmail.com>
2023-01-05 16:02:12 +0200

    test6

--- src/controlers/products.ts
+++ src/controlers/products.ts
@@ -3,7 +3,7 @@
 import { Request, Response } from 'express';
 import * as productServices from '../services/products';
 
-export const getAll = async (req: Request, res: Response) => {
+export const getAll = async (req, res) => {
   const phones = await productServices.getAll();
 
   res.send(phones);
--- src/services/products.ts
+++ src/services/products.ts
@@ -3,7 +3,8 @@ import fs from 'fs/promises';
 // import { Product } from '../types/Product';
 
 export const getAll = async () => {
-  return fs.readFile('./src/api/phones.json', 'utf-8')
+  return fs
+    .readFile('./src/api/phones.json', 'utf-8')
     .then((data) => JSON.parse(data))
     .catch((error) => console.log(error, 'error'));
 };

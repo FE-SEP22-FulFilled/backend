43611ce78ae34bdcea6ffc557a59ac033266b478
andriy-shymkiv <winamondezik@gmail.com>
2023-01-05 20:05:30 +0200

    pagination was added

--- src/controlers/products.ts
+++ src/controlers/products.ts
@@ -1,10 +1,37 @@
-/* eslint-disable */
-
+/* eslint-disable no-console */
 import { Request, Response } from 'express';
 import * as productServices from '../services/products';
 
 export const getAll = async (req: Request, res: Response) => {
-  const phones = await productServices.getAll();
+  try {
+    const phones = await productServices.getAll();
+    const { quantity } = req.query;
+    let int = Number(quantity);
 
-  res.send(phones);
+    if (!int) {
+      int = 16;
+    }
+
+    const displayedPhones = phones?.filter(phone => {
+      switch (int) {
+        case 16:
+          return phone.id <= 16;
+
+        case 8:
+          return phone.id <= 8;
+
+        case 4:
+          return phone.id <= 4;
+
+        default:
+          return phone.id <= 16;
+      }
+    });
+
+    res.send(displayedPhones);
+  } catch (error) {
+    console.log(error);
+
+    res.sendStatus(404);
+  }
 };
--- src/services/products.ts
+++ src/services/products.ts
@@ -1,8 +1,8 @@
 /* eslint-disable no-console */
 import fs from 'fs/promises';
-// import { Product } from '../types/Product';
+import { Product } from '../types/Product';
 
-export const getAll = async () => {
+export const getAll = async (): Promise<Product[] | null> => {
   return fs
     .readFile('./src/api/phones.json', 'utf-8')
     .then((data) => JSON.parse(data));

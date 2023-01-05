9ada608047aa94a1bcb991fcd8f3b8afbcbb4258
andriy-shymkiv <winamondezik@gmail.com>
2023-01-05 14:01:10 +0200

    test3

--- src/controlers/products.ts
+++ src/controlers/products.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import * as productServices from '../services/products';
 
-export const getAll = async (req: Request, res: Response) => {
+export const getAll = async (_req: Request, res: Response) => {
   const phones = await productServices.getAll();
 
   res.send(phones);

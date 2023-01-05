703b0f25bf60c95136cbcd6e92b85fe8f9b1e6e8
andriy-shymkiv <winamondezik@gmail.com>
2023-01-05 15:11:46 +0200

    test11

--- src/controlers/products.ts
+++ src/controlers/products.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 
-import { Request, Response } from 'express';
+// import { Request, Response } from 'express';
 import * as productServices from '../services/products';
 
-export const getAll = async (req: Request, res: Response) => {
+export const getAll = async (req, res) => {
   const phones = await productServices.getAll();
 
   res.send(phones);

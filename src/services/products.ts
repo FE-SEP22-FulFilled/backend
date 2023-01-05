b9959cbde0cac63fcdf124f5702ae74383c3db60
andriy-shymkiv <winamondezik@gmail.com>
2023-01-05 14:59:16 +0200

    test10

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 // import { Product } from '../types/Product';
 
-export const getAll = async () => {
+export const getAll = () => {
   const phones = fs.readFileSync('./src/api/phones.json', 'utf-8');
 
   return JSON.parse(phones);

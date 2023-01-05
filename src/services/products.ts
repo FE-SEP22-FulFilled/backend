a5fbc71243eb85cd5d388fc7fb1dec16976071de
andriy-shymkiv <winamondezik@gmail.com>
2023-01-05 16:00:21 +0200

    test5

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,8 +1,9 @@
-import fs from 'fs';
+/* eslint-disable no-console */
+import fs from 'fs/promises';
 // import { Product } from '../types/Product';
 
 export const getAll = async () => {
-  const phones = fs.readFileSync('src/api/phones.json', 'utf-8');
-
-  return JSON.parse(phones);
+  return fs.readFile('./src/api/phones.json', 'utf-8')
+    .then((data) => JSON.parse(data))
+    .catch((error) => console.log(error, 'error'));
 };

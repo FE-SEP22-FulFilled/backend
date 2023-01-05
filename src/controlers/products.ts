d88a09c9c393bd77ba90ae01c2bcb1d8fffa1bcf
andriy-shymkiv <winamondezik@gmail.com>
2023-01-05 13:45:20 +0200

    fix1

--- src/controlers/products.ts
+++ src/controlers/products.ts
@@ -2,6 +2,9 @@ import { Request, Response } from 'express';
 import * as productServices from '../services/products';
 
 export const getAll = async (req: Request, res: Response) => {
+  // eslint-disable-next-line no-console
+  console.log(req);
+
   try {
     const phones = await productServices.getAll();
 
